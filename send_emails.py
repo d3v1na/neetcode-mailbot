56e9fe7c2d2b342bb876eb33dc4cb3f88cea6475
Devina Bhatnagar <db133@snu.edu.in>
2023-12-29 00:57:55 +0530

    email part

--- send_emails.py
+++ send_emails.py
@@ -4,3 +4,30 @@ from email.message import EmailMessage
 from email.utils import formataddr 
 from pathlib import Path 
 
+from dotenv import load_dotenv
+
+PORT = 587
+SMTP_SERVER = "smtp.gmail.com"
+
+#load the environment variables
+current_dir = Path(__file__).resolve().parent if "__file__" in locals() else Path.cwd()
+envars = current_dir / ".env"
+load_dotenv(envars)
+
+#read the environment variables
+sender_email = os.getenv("EMAIL")
+sender_password = os.getenv("PASSWORD")
+
+def send_email(receiver_email, subject, body):
+    message =  EmailMessage()
+    message["Subject"] = subject
+    message["From"] = formataddr(("Devina", f{sender_email}))
+    message["To"] = receiver_email
+    message.set_content(
+        f"""\
+        hope this email finds you well.
+        warm regards, 
+        devina
+        """
+    )
+
